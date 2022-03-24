9bd03a9afc4ffe94f7f3526585e7cda98dac979c
P.angelino <p.angelinodaniele@gmail.com>
2022-03-24 13:07:20 +0100

    inizio

---
+++ js-mail/main.js
@@ -0,0 +1,16 @@
+
+/*Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, 
+stampa un messaggio appropriato sull’esito del controllo. La lista delle email accettate è:
+matteo@gmail.com
+fabio@gmail.com
+pietro@gmail.com
+anna@gmail.com
+
+*/
+
+let email = prompt("Inserisci email");
+
+
+
+
+
