df0ace1e284ce28009c0129bb03ff4a00c57c2c0
P.angelino <p.angelinodaniele@gmail.com>
2022-03-25 13:55:31 +0100

    .

--- js-mail/main.js
+++ js-mail/main.js
@@ -15,13 +15,13 @@ let arrayEmail = ["matteo@gmail.com", "fabio@gmail.com", "pietro@gmail.com", "an
 
 
 
-for(let i = 0; i<arrayEmail.length; i++){
+for(let i = 0; i<=arrayEmail.length; i++){
     if(arrayEmail[i]==email){
         i=arrayEmail.length;
         alert("Puoi accedere al contenuto");
         
     }else if(arrayEmail[i]!=email && i==arrayEmail.length){
-         addEmail = prompt("Vuoi aggiungere questa email?");
+         addEmail = prompt("Vuoi aggiungere questa email? Rispondi si per aggiungere, no per non aggiungere");
     }
     
     
@@ -31,4 +31,6 @@ for(let i = 0; i<arrayEmail.length; i++){
 if(addEmail == "si" ){
     arrayEmail.push(email)
 }
-console.log(arrayEmail);
+for(let i =0; i<arrayEmail.length; i++){
+    console.log(arrayEmail[i]);
+}
